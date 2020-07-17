5c46c911c602cb180efe3ac0a00e747602aaf489
ChachyDev <shalomadecoolboy@outlook.com>
2020-07-17 07:28:53 +0100

    🎉 Initial Commit

---
+++ src/main/kotlin/dev/minecraftbox/launch/BoxTweaker.kt
@@ -0,0 +1,26 @@
+package dev.minecraftbox.launch
+
+import net.minecraft.launchwrapper.ITweaker
+import net.minecraft.launchwrapper.LaunchClassLoader
+import java.io.File
+
+@Suppress("unused")
+class BoxTweaker : ITweaker {
+    private val boxArguments = arrayListOf<String>()
+
+    override fun acceptOptions(args: MutableList<String>, gameDir: File, assetsDir: File, profile: String) {
+        boxArguments += args + listOf(
+            "--gameDir", gameDir.absolutePath,
+            "--assetsDir", assetsDir.absolutePath,
+            "--version", profile
+        )
+    }
+
+    override fun getLaunchTarget() = "net.minecraft.client.main.Main"
+
+    override fun injectIntoClassLoader(classLoader: LaunchClassLoader) {
+
+    }
+
+    override fun getLaunchArguments(): Array<String> = boxArguments.toTypedArray()
+}
