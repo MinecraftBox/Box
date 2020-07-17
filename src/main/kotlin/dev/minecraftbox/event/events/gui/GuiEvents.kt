5538860c8fdefcf59054878bf9c7c95a77a2f16a
ChachyDev <shalomadecoolboy@outlook.com>
2020-07-17 13:38:35 +0100

    :construction: Make the GuiEvents cancelable

--- src/main/kotlin/dev/minecraftbox/event/events/gui/GuiEvents.kt
+++ src/main/kotlin/dev/minecraftbox/event/events/gui/GuiEvents.kt
@@ -1,7 +1,8 @@
 package dev.minecraftbox.event.events.gui
 
+import dev.minecraftbox.event.CancellableEvent
 import net.minecraft.client.gui.GuiScreen
 
-class GuiOpenEvent(val guiScreen: GuiScreen)
+class GuiOpenEvent(val guiScreen: GuiScreen) : CancellableEvent()
 
-class GuiCloseEvent
+class GuiCloseEvent : CancellableEvent()
