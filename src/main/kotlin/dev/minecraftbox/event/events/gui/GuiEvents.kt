536e0e7f29222b75a38acd54db4b13f6dbfc8bb7
ChachyDev <shalomadecoolboy@outlook.com>
2020-07-17 13:35:21 +0100

    :rewind: Revert

--- src/main/kotlin/dev/minecraftbox/event/events/gui/GuiEvents.kt
+++ src/main/kotlin/dev/minecraftbox/event/events/gui/GuiEvents.kt
@@ -1,8 +1,7 @@
 package dev.minecraftbox.event.events.gui
 
-import dev.minecraftbox.event.CancellableEvent
 import net.minecraft.client.gui.GuiScreen
 
-class GuiOpenEvent(val guiScreen: GuiScreen) : CancellableEvent()
+class GuiOpenEvent(val guiScreen: GuiScreen)
 
-class GuiCloseEvent : CancellableEvent()
+class GuiCloseEvent
