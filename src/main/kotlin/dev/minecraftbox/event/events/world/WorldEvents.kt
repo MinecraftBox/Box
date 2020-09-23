acbdb08e1ef5f99dc3b31d5bc2c58317f444063d
ChachyDev <shalomadecoolboy@outlook.com>
2020-09-23 17:11:28 +0100

    Fix a bug where the game crashes on world join.

--- src/main/kotlin/dev/minecraftbox/event/events/world/WorldEvents.kt
+++ src/main/kotlin/dev/minecraftbox/event/events/world/WorldEvents.kt
@@ -3,4 +3,4 @@ package dev.minecraftbox.event.events.world
 import dev.minecraftbox.event.CancellableEvent
 import net.minecraft.client.multiplayer.WorldClient
 
-class WorldLoadEvent(val worldClient: WorldClient, val loadingMessage: String) : CancellableEvent()
+class WorldLoadEvent(val worldClient: WorldClient?, val loadingMessage: String) : CancellableEvent()
