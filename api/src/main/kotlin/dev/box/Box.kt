a7d77ea3f314e7a87673cc24a869d62c57cab141
ChachyDev <shalomadecoolboy@outlook.com>
2021-01-10 20:42:18 +0000

    Stop returning when directory isnt found

--- api/src/main/kotlin/dev/box/Box.kt
+++ api/src/main/kotlin/dev/box/Box.kt
@@ -22,7 +22,7 @@ object Box {
         logger.info("Marking Box API as started. Loading for Minecraft ${minecraft.version}")
         TitleManager.addTitle("Box $VERSION")
         logger.info("Starting mod discovery")
-        ModDiscovery.discover(boxDir, minecraft)
+        ModDiscovery.discover(boxDir, minecraft, logger)
         logger.info("Invoke mod loading")
         Loader.loadMods(ModDiscovery.discoveredMods)
         state = State.LAUNCHING
--- modloader/src/main/kotlin/dev/box/loader/discovery/ModDiscovery.kt
+++ modloader/src/main/kotlin/dev/box/loader/discovery/ModDiscovery.kt
@@ -1,20 +1,18 @@
 package dev.box.loader.discovery
 
 import clone.net.minecraft.client.Minecraft
-import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import java.io.File
 
 object ModDiscovery {
-    val logger: Logger = LogManager.getLogger(this)
-
     var discoveredMods = mutableListOf<File>()
 
-    fun discover(directory: File, minecraft: Minecraft) {
-        val filesToLoad = directory.listFiles()?.filter { it.extension == "jar" }?.toMutableList() ?: return
-        logger.info("Found ${filesToLoad.size} mods(s) to load from main mod directory")
-        val versionFiles = File(directory, minecraft.version).listFiles()?.filter { it.extension == "jar" } ?: return
-        logger.info("Found ${versionFiles.size} mod(s) to load from version specific directory.")
+    fun discover(directory: File, minecraft: Minecraft, logger: Logger) {
+        if (!directory.exists()) directory.mkdir()
+        val filesToLoad = directory.listFiles()?.filter { it.extension == "jar" }?.toMutableList() ?: mutableListOf()
+        logger.info("Found ${filesToLoad.size} mod(s) to load from main mod directory")
+        val versionFiles = File(directory, minecraft.version).listFiles()?.filter { it.extension == "jar" } ?: mutableListOf()
+        logger.info("Found ${versionFiles.size} mod(s) to load from version specific directory (${directory.name}/${minecraft.version}).")
         filesToLoad.addAll(versionFiles)
         logger.info("Setting discoveredMods to discovered mods.")
         discoveredMods = filesToLoad
